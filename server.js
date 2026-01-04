cf37ce629a8a21526bb8cf0c228234cf590c141e
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2026-01-04 16:11:32 -0500

    pages and routing added

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const http = require("http");
+const fs = require("fs");
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  console.log(req.url, req.method);
 
-  res.write("hello world");
-  res.end();
+  let path = "./views";
+  path += req.url;
+  path += ".html";
+
+  if (req.url == "/") {
+    res.setHeader("Location", "/index");
+    res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        console.log(err);
+        fs.readFile("./views/404.html", (err, data) => {
+          if (err) {
+            console.log(err);
+            res.end();
+          } else {
+            res.end(data);
+          }
+        });
+      } else {
+        res.end(data);
+      }
+    });
+  }
 });
 
 server.listen(port, "localhost", () => {
